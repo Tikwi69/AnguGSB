ca2529bac6c5f771ca90cb6e7a0c07644766b4c8
Virgile Bastasi <virgile.bastasi@lmdsio.fr>
2022-11-24 15:34:50 +0100

    arkunir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ListefichefraisComponent } from '../listefichefrais/listefichefrais.component';
-import { FichefraisComponent } from '../fichefrais/fichefrais.component';
-import { AjoutfichefraisComponent } from '../ajoutfichefrais/ajoutfichefrais.component';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { ConnexionComponent } from '../connexion/connexion.component';
+import { ListefichefraisComponent } from './listefichefrais/listefichefrais.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   { path: '', redirectTo : '/connexion', pathMatch: 'full' },
-  { path: 'connexion', component : ConnexionComponent },
   { path: 'Listefichefrais', component : ListefichefraisComponent },
-  { path: 'modifierFrais/:id', component : FichefraisComponent },
-  { path: 'ajouterFrais', component : AjoutfichefraisComponent },
   { path: 'accueil', component : NavbarComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ListefichefraisComponent } from './listefichefrais/listefichefrais.component';
+import { FicheFraisService} from "./service/fichefrais.service";
+import { Fichefrais} from "./metier/fichefrais";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,10 +17,14 @@ import { ListefichefraisComponent } from './listefichefrais/listefichefrais.comp
     ListefichefraisComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    FicheFraisService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
