800fe880e0e0a601c34746c3d08d56d6d7a98a3c
Virgile Bastasi <virgile.bastasi@lmdsio.fr>
2022-12-01 15:12:59 +0100

    arkunir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ListefichefraisComponent } from './listefichefrais/listefichefrais.component';
 import { FichefraisService} from "./service/fichefrais.service";
-import { Fichefrais} from "./metier/fichefrais";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ListefichefraisComponent
+    ListefichefraisComponent,
+    ConnexionComponent
   ],
   imports: [
     FormsModule,
